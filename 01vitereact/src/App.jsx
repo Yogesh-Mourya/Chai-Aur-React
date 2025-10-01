3c035ab2f4f0696212a470e023377f0303c01038
Yogesh <yashmourya541@gmail.com>
2025-10-02 03:31:52 +0530

    Lecture 4

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,11 +1,14 @@
 import Chai from "./Chai"
-function App() {
 
+function App() {
+  const username = "Chai aur Code"
   return (
+    // {username} is evaluated expression i.e we dont write js here,
+    // here we get js evaluated result , after tree formation this are injected
     /* fragment */
     <> 
       <Chai/>
-      <h1>Chai aur react</h1>
+      <h1>Chai aur react | {username}</h1> 
       <p>test Para</p>
     </>
   )
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,45 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+function MyApp(){
+  return (
+    <div>
+      <h1>Custom App</h1>
+    </div>
+  )
+}
+
+// const reactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'https://www.google.com', 
+//         target: '_blank', 
+//     },
+//     children: 'Click here to visit Google'
+// }
+
+const anotherElement = (
+  <a href="https://www.google.com" target="_blank"> Visit Google</a>
+)
+
+const anotheruser = "Chai aur Code"
+//Vimp : JSX gets converted to React.createElement calls
+// internal tree structure
+const reactElement = React.createElement(
+  'a', 
+  {href: 'https://www.google.com', target: '_blank'}, 
+  'Click here to visit Google',
+  anotheruser // evaluated expression (js) after tree formation is placed here
+) 
+
+ReactDOM.createRoot(document.getElementById('root')).
+render(
+//  <>
+//     <App />
+ 
+//  </>
+  // MyApp()
+  // anotherElement
+  reactElement
 )
