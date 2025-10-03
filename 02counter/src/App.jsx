45fd437a6d54e812e954310f737c9de177f01c28
Yogesh <yashmourya541@gmail.com>
2025-10-03 16:11:08 +0530

    Lecture 8 Interview Question on Counter

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -8,16 +8,35 @@ function App() {
   let [counter, setCounter] = useState(15);
   // let counter = 15
 
+  // const addValue = () => {
+  //   if (counter < 20) {
+  //     counter = counter + 1;
+  //     setCounter(counter);
+  //     console.log("Value Added: ",counter);
+  //   }
+  //   else {
+  //     alert("Counter cannot exceed 20");
+  //   }
+  // }
+
+  //Interview Question on counter
   const addValue = () => {
-    if (counter < 20) {
-      counter = counter + 1;
-      setCounter(counter);
-      console.log("Value Added: ",counter);
-    }
-    else {
-      alert("Counter cannot exceed 20");
-    }
-  }
+    setCounter(counter + 1);
+    setCounter(counter + 1);
+    setCounter(counter + 1);
+    setCounter(counter + 1);
+  }// what will be the value of counter if we click on AddValue button once? Ans: 16
+
+  /*
+    setCounter((prevCounter) => prevCounter + 1);
+    setCounter((prevCounter) => prevCounter + 1);
+    setCounter((prevCounter) => prevCounter + 1);
+    setCounter((prevCounter) => prevCounter + 1);
+  */
+  //Answer: 19(Reason Batch algorithm), because react batches the state updates for performance reasons. 
+  //When you call setCounter multiple times in a single event handler, React doesn't immediately update the state. 
+  //Instead, it waits until the event handler has finished executing and then processes all the state updates at once.
+  //To ensure that each update is based on the most recent state, you can use the functional form of the state updater:
 
   const removeValue = () => {
     if (counter > 0) {
